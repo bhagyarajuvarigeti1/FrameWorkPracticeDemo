09af11b8b16ba83be3b5c4caf4cd82da6518c8c2
bhagyarajuvarigeti1 <104958745+bhagyarajuvarigeti1@users.noreply.github.com>
2024-05-27 15:19:00 +0530

    add file

---
+++ IOS_Network_Services.xcframework/ios-arm64_x86_64-simulator/IOS_Network_Services.framework/Headers/IOS_Network_Services.h
@@ -0,0 +1,18 @@
+//
+//  IOS_Network_Services.h
+//  IOS_Network_Services
+//
+//  Created by Lekkala Sai Himaja on 27/05/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IOS_Network_Services.
+FOUNDATION_EXPORT double IOS_Network_ServicesVersionNumber;
+
+//! Project version string for IOS_Network_Services.
+FOUNDATION_EXPORT const unsigned char IOS_Network_ServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IOS_Network_Services/PublicHeader.h>
+
+
